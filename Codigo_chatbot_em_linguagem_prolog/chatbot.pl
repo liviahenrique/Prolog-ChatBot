675642201644cec986ab82901b715260070ceff7
liviahenrique <74997077+liviahenrique@users.noreply.github.com>
2021-05-23 20:44:34 -0300

    committ do Código em linguagem

---
+++ Codigo_chatbot_em_linguagem_prolog/chatbot.pl
@@ -0,0 +1,155 @@
+%Aluna: Livia Maria Henrique
+%RGM:22211411
+
+%PROJETO INTEGRADOR DE COMPETÊNCIAS EM CIÊNCIA DA COMPUTAÇÃO IV
+
+% Base de Dados I(chatbot_perguntas)
+
+
+chatbot_perguntas( "rede de computadores","Uma rede de computadores pode ser definida como  uma estrutura de computadores e dispositivos conectados através de um sistema de comunicação com o objetivo, de compartilharem informações e recursos entre si.").
+
+chatbot_perguntas("classificacao de redes","Podemos classificar as redes quanto a:Topologia Física(Barramento,anel, estrela,malha,arvore,hibrida).Area de abrangência(Lan,Man,Wan).Tecnologia de Comutação(Circuitos, mensagens, pacotes).
+Tipo de conexão(ponto a ponto, cliente-servidor).").
+
+chatbot_perguntas("roteador","O roteador é o aparelho responsável por fazer a ponte entre o modem/ONU e os equipamentos, sem o uso de cabos, ou seja, é ele que confere o sinal Wi-Fi. Sua função é buscar os melhores caminhos
+(mais curtos e menos congestionados) para conseguir enviar e receber dados entre os dispositivos  conectados e a rede.").
+
+chatbot_perguntas("placa de rede","Uma placa de rede (também chamada adaptador de rede ou NIC, do acrônimo inglês Network Interface
+Card) é um dispositivo de hardware responsável pela comunicação entre os computadores de uma rede. A placa de rede é o hardware que
+permite aos computadores conversarem entre si através da rede.A  sua função é controlar todo o envio e  recepção de dados através da rede.Cada  arquitetura de rede exige um tipo específico de placa de rede; sendo as arquiteturas mais comuns a rede em anel Token Rin e a tipo Ethernet.").
+
+chatbot_perguntas("modem","O modem é o aparelho responsável pela conexão do seu dispositivo ao serviço oferecido pelo provedor de internet. Utilizado nas conexões via cabo, esse aparelho recebe o sinal da rede e distribui para os aparelhos por meio de um cabo
+Ethernet, conectado diretamente no computador ou ao roteador.").
+
+chatbot_perguntas("repetidor","O repetidor de sinal é um aparelho que recebe o sinal gerado pelo seu roteador e o amplifica, permitindo que ele alcance áreas maiores. Ele é frequentemente utilizado por pessoas que desejam obter sinal de cômodos distantes da casa ou até por estabelecimentos que desejam ter uma cobertura maior para oferecer aos seus clientes.").
+
+chatbot_perguntas("switch","O switch é uma aparelho que irá conectar todos os computadores da sua rede, de forma que eles possam trocar dados entre si. O switch recebe os dados do computador de origem e redireciona para o computador de destino, seja ele um servidor ou um desktop normal, de um outro colaborador que está conectado na mesma rede.").
+
+chatbot_perguntas("lan","Local Area Network, ou rede local, é a configuração para redes instaladas em áreas menores, como casas e
+escritórios pequenos. Na porta LAN do roteador o usuário pode conectar computadores, notebooks e periféricos para permitir a distribuição e transmissão da conexão de Internet.").
+
+chatbot_perguntas("man","Sigla de Metropolitan Area Network pode ser traduzida como Rede Metropolitana. A função dessa rede é fazer aconexão entre diferentes Redes Locais que estejam num raio de algumas dezenas de quilômetros. Uma ótima solução para empresas que temduas unidades numa mesma cidade, por exemplo, e que deseja interligar seus computadores. Interliga computadores e utilizadores num raio geográfico mais amplo do que uma LAN, mas menor do que a área de uma WAN.").
+
+chatbot_perguntas("wan","Essa sigla significa Wide Area Network podendo ser chamada de Rede de Longa Distância e consegue uma abrangência mais ampla do que a MAN. É possível que essa rede conecte Redes Locais em países e até em continentes distintos.Indicado para
+quem precisa de um alinhamento entre redes com uma grande distância entre si. Uma rede WAN geralmente é o resultado da junção de
+diversas redes LAN numa mesma cidade dando origem assim a uma rede de maior porte.").
+
+chatbot_perguntas("internet","A internet é uma rede mundial que tem como objetivo interligar computadores para fornecer ao usuário o acesso a diversas informações. Por isso é chamada de rede mundial de computadores.").
+
+chatbot_perguntas("correio eletronico","O correio electrónico (igualmente conhecido pelo nome de e-mail, que é a forma abreviada do
+termo inglês electronic mail) é um serviço que permite trocar mensagens através de sistemas de comunicação eletronicos.As mensagens
+de correio eletronico possibilitam o envio, não só de textos, como de qualquer tipo de documento digital (imagens, vídeos,
+áudios,etc").
+
+chatbot_perguntas("coaxial","Cabo coaxial é uma espécie de cabo condutor usado para a transmissão de sinais. Ele recebe tal nome por ser constituído de várias camadas concêntricas de condutores e isolantes.  O cabo coaxial é basicamente formado por  um fio de cobre condutor revestido por um material isolante, e ainda rodeado por uma blindagem.").
+
+chatbot_perguntas("par trançado","Os cabos de par trançado são meios físicos guiados que
+permitem a transmissão de dados em redes modernas. Possuem pares de fios metálicos entrelaçados entre si de modo a reduzir
+interferências eletromagnéticas e o efeito de crosstalk (interferência de um sinal sobre outro).").
+
+chatbot_perguntas("optica","A fibra óptica é uma tecnologia de transmissão de dados em alta velocidade. São cabos feitos de material transparente e reflexivo,e podem ser tão finos quanto o cabelo humano. Este tipo de fibra é utilizada principalmente por companhias
+de telecomunicações, devido ao seu alto grau de segurança na entrega da informação. A tecnologia que proporciona o uso da fibra óptica é essencial para a expansão das tecnologias digitais,permitindo uma comunicação ininterrupta e rápida de sinais e dados, seja por
+voz ou vídeo.").
+
+chatbot_perguntas("radiofrequencia","As ondas de radiofrequência estão muito presentes em nosso cotidiano em transmissões de áudio, televisão,internet, entre outros. Essas ondas são eletromagnéticas(ondas que não precisam de um meio de propagação) e possuem
+frequências menores que a da luz visível, estando compreendidas no intervalo de 3 KHz a 300 Ghz. Dependendo de suas características, podem ser chamadas de AM, FM, UHF ou VHF.").
+
+chatbot_perguntas("topologia de rede","A topologia de rede é o padrão no qual o meio de rede está conectado aos computadores e outros
+componentes de rede. Essencialmente, é a estrutura topológica da rede, e pode ser descrito fisicamente ou logicamente").
+
+chatbot_perguntas("parcialmente ligada","Nesse tipo de tecnologia,os computadores são ligados de forma intermediaria,ou seja não são
+todos os computadores que são interligados,mas existem caminhos alternativos que podem ser usados em casos de falhas ou
+congestionamentos.").
+
+chatbot_perguntas("barramento","A topologia de barramento também conhecida como barramento  linear. Este é o método mais simples e
+comum de conectar os computadores em rede. Constituem em um único cabo, chamado tronco (e também backbone ou segmento), que conecta
+todos os computadores da rede em uma linha única.").
+
+chatbot_perguntas("ponto","é um formato de redes de computadores onde cada um dos pontos da  rede funciona tanto como cliente quanto como servidor, permitindo  compartilhamentos de serviços e dados sem a necessidade de um servidor central. A topologia ponto a ponto é a mais simples.").
+
+chatbot_perguntas("multiponto","Conexões de rede multiponto são aquelas nas quais mais de dois dispositivos são interligados usando apenas uma conexão. A rede multiponto é muitas vezes referida como circuito multiponto ou ainda multidrop.De modo a permitir que todosos dispositivos conversem entre si, a capacidade da conexão pode ser dividida de forma temporal e espacial entre os dispositivos da
+rede.").
+
+chatbot_perguntas("estrela","Nesta topologia existe um dispositivo central,Hub ou Switch (que pode ser activo ou passivo), ao qual seligam os vários computadores da Rede, através de cabos individuais.outros computadores da Rede. O acrescento ou retirada de um
+computador da rede é realizado facilmente, pois basta inserir ou retirar o cabo de ligação desse computador a um conector do
+dispositivo central(Hub ou Switch). O Hub ou Switch têm um determinado número de portas, mas podem interligar-se vários Hubs de
+forma a aumentar o número de computadores a ligar.").
+
+%Base de Dados II (chatbot_perguntas)
+
+chatbot_perguntas("ethernet","Ethernet é um tipo de protocolo de rede, que determina a linguagem utilizada pelos dispositivos para
+trocar informações. Essa comunicação é feita através de dos chamados cabos de rede, ou cabos ethernet. São eles que realizam a conexão em circuito entre os dispositivos e diversos aparelhos, como roteadores, hubs e outros componentes de hardware").
+
+chatbot_perguntas("endereçamento","A Ethernet possui um sistema de endereçamento de 48 bits. A cada computador conectado a uma rede
+Ethernet é projetado um único número de 48 bits conhecido como o endereço da Ethernet.Um serviçoEthernet é o estabelecimento de um
+EVC onde o cliente se conecta no provedor de serviços através de um equipamento (CE - Customer Equipament)se ligando por uma
+interface UNI à rede Metro Ethernet. Esta conexão pode ser feita utilizando um dos padrões ethernet, 10Mbps, 100Mbps,1Gbps ou 10Gbps.O cliente pode contratar dois tipos de serviços que são o ponto-a-ponto ou o multiponto-multiponto e a partir do   tipo de serviço o cliente deverá especificar os parâmetros  de cada um dos atributos do serviço que será
+contratado.").
+
+chatbot_perguntas("mac","Media Access Control( Controle de Acesso ao Meio) é um termo utilizado em redes de computadores para
+designar parte da camada de enlace, camada número 2 segundo o modelo OSI. É provedora de acesso a um canal de comunicação e o
+endereçamento neste canal possibilitando a conexão de diversos computadores numa mesma rede.").
+
+chatbot_perguntas("gigabit","O padrão Gigabit Ethernet (ou 10/100/1000) foi criado em 1999 e continua sendo o tipo de rede mais
+recente.Seu nome vem exatamente da velocidade prometida, de 1 Gbps, que é 10 vezes maior  àquela oferecida pelo Fast Ethernet. A qualidade da conexão é uma das suas principais vantagens em relação aos seus antecessores, mas é importante destacar que esse é um valor teórico. Com bons equipamentos e instalação correta, o mais comum é chegar a cerca de 900 Mbps.").
+
+chatbot_perguntas("metro","Metro Ethernet nada mais é do que o conceito de utilização de redes do padrão Ethernet para áreas
+metropolitanas e geograficamente distribuídas, sempre considerando a diminuição de custos e a efetividade.Os maiores triunfos do Metro Ethernet são a simplicidade e a facilidade de operação aliadas ao alto grau de integração e padronização do protocolo. Ou seja, trata-se de uma tecnologia muito atrativa por ser eficiente e barata.").
+
+chatbot_perguntas("TCP/IP","O TCP/IP é um conjunto de protocolos de comunicação. O nome vem de dois protocolos TCP (Transmission Control Protocol) e o IP (Internet Protocol). Ele tem por objetivo padronizar todas as comunicações de rede, principalmente as
+comunicações na web.").
+
+chatbot_perguntas("protocolos","Os protocolos TCP/IP são divididos em 4 camadas:aplicação: usada para enviar e receber dados de
+outros programas pela internet. Nessa camada estão os protocolos HTTP, FTP e SMTP;transporte: responsável por transportar os arquivos
+dos pacotes recebidos da camada de aplicação. Eles são organizados e transformados em outros menores, que serão enviados à rede;
+rede: os arquivos empacotados na camada de transporte são recebidos e anexados ao IP da máquina que envia e recebe os dados. Em seguida,eles são enviados pela internet;interface: é a camada que executa o recebimento  ou o envio de arquivos na web.").
+
+chatbot_perguntas("IGRP","Protocolo Interno de Distribuição é um protocolo de distribuição de dados do gateway dentro de uma complexarede de computadores com várias rotas paralelas, desenvolvido na década de 1980 pela Cisco Systems.").
+
+chatbot_perguntas("EIGRP","O EIGRP é um protocolo avançado de roteamento por vetor da distância, desenvolvido primeiramente como um
+protocolo proprietário da Cisco. Funcionalidades básicas do EIGRP foram convertidas para padrão aberto em 2013, e foram especificadasna RFC 7868 em 2016. O EIGRP é uma evolução do seu antecessor, o IGRP.").
+
+chatbot_perguntas("SMTP","O SMTP (Simple Mail Transfer Protocol) é a tecnologia que permite que e-mails sejam enviados de um servidor
+para outro servidor até que eles sejam entregues na sua caixa de mensagens.Em outras palavras, o SMTP é um protocolo de internet que
+conecta máquinas e ajuda a transportar o seu e-mail. É o seu carteiro virtual.").
+
+chatbot_perguntas("telnet","O Telnet é um protocolo e aplicação, parte da família TCP/IP, para acesso e utilização de computadores remotos.Com Telnet podemos fazer login noutros computadores da Internet e utilizar os seus recursos. Por exemplo, executar aplicações
+e/ou aceder a serviços  existentes nos computadores remotos., consultar bases de dados e catálogos bibliográficos (OPAC's), etc. O
+nosso computador passa a ser como que um terminal (não inteligente) directamente ligado ao computador remoto onde é executado todo o
+processamento").
+
+chatbot_perguntas("FTP","FTP é a sigla para File Transfer Protocol, um termo que, traduzido para o português, significa Protocolo de Transferência de Arquivos.Ele é basicamente um tipo de conexão que permite a troca de arquivos entre dois computadores conectados à
+internet. Com isso, você pode enviar qualquer coisa para uma outra máquina ou armazená-los em um servidor FTP, ficando ela sempre
+disponível para o usuário acessar.").
+
+chatbot_perguntas("http","HTTP é um protocolo de transferência que possibilita que as pessoas que inserem a URL do seu site na
+Web possam ver os conteúdos e dados que nele existem. A sigla vem do inglês Hypertext Transfer Protocol.").
+
+chatbot_perguntas("camada de rede","A camada de rede é responsável por prover conectividade ao computador e selecionar caminhos para que os pacotes de dados possam trafegar. Isso é feito através do roteamento. Um motivo para que a conectividade possa ocorrer tem
+relação com o IP (Internet Protocol). O endereçamento IP permite que o dispositivo possa se conectar em uma mesma rede ou
+em redes diferentes.").
+
+chatbot_perguntas("IPv4","IPv4 significa Protocol version 4, ou versão 4 de protocolos. É a tecnologia que permite que nossos aparelhos conectem na Internet, seja qual for o tipo de gadget – pode ser PC, Mac, smartphones ou outros aparelhos. Cada um que estiver online terá um código único, como 99.48.227.227 por exemplo, para enviar e receber dados de outros que estiverem conectados.").
+
+chatbot_perguntas("ICMP","Internet Control Message Protocol (ICMP)) uma rede de protocolo que é responsável por comunicar erros
+através dos meios de geração e envio de mensagens para o endereço IP de origem quando existem problemas de rede encontrados pelo
+sistema. As mensagens que o ICMP gera indicam que um determinado gateway, roteador, serviço ou mesmo host que deveria se conectar à
+internet não pode ser alcançado. Basicamente, o destinatário não pode receber pacotes durante a transmissão. Qualquer dispositivo de rede IP pode enviar,gerar, receber e processar mensagens de erro ICMP.").
+
+% Regras para realizar as consultas
+
+ chatbot:-write("Seja bem vindo ao nosso chat para dúvidas relacionadas a Redes de Computadores e Internet"),nl,
+ write("Antes de Inicia com as perguntas"),nl,
+ write("(Para o Sistema Entender sua pergunta escreva a palavra chave com letra minuscula e entre aspas Duplas e não esqueça de coloca um ponto final depois fechar as Aspas Duplas, Obrigada :-))"),nl,
+write("Qual Seria Sua duvida ?"),nl,
+ read(X),chatbot_perguntas(X,Y),write(Y),nl,
+  repeat,
+ write("Deseja continuar ?"),nl,
+ read(P),
+ (P==nao,write("Obrigado, Até Mais! Foi otimo conversar com voce");
+      processar(P),fail
+ ).
+  processar(P):-P == sim, write("(Para o Sistema Entender sua pergunta escreva a palavra chave com letra minuscula e entre aspas Duplas e não esqueça de coloca um ponto final depois das Aspas Duplas, Obrigada :-))"),nl,
+write("Qual Seria Sua duvida ?"),nl,
+ read(X),chatbot_perguntas(X,Y),write(Y),nl.
+processar(P):-P\=sim,write("Opção invalida!"),nl,write("Digite sim ou nao"),nl.
